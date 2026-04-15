07967a16fc7f1e8d658e4278aba6db9a9c6803fd
marco romano fina <marco@Mac.fibertel.com.ar>
2026-04-15 01:59:26 -0300

    initial data from jazzlogs weeks 1 - 3 and project setup

---
+++ src/test/java/com/marcoromanofinaa/jazzlogs/JazzlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoromanofinaa.jazzlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazzlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
